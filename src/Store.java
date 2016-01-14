c0fa815c730febc80a4740aaa912e7ce0707da55
Piotr-Majewski <piotrmajewski1@wp.pl>
2016-01-14 19:01:03 +0100

    if loop changed to switch instruction

--- src/Store.java
+++ src/Store.java
@@ -5,14 +5,14 @@ public class Store {
 	public static void main(String[] args) {
 		Store myStore = new Store();
 		myStore.init();
-
 	}
 
 	public void init() {
 
 		Scanner input = new Scanner(System.in);
-		Basket standardList = new Basket();
+		Basket standardBasket = new Basket();
 
+		outerLoop:
 		while (true) {
 
 			System.out
@@ -20,31 +20,33 @@ public class Store {
 
 			String inputCommand = input.next();
 
-			if (inputCommand.equals("add")) {
+			switch (inputCommand) {
 
+			case "add":
 				String inputName = input.next();
 				double inputPrice = input.nextDouble();
-				standardList.addProduct(new Product(standardList.getSize(),
+				standardBasket.addProduct(new Product(standardBasket.getSize(),
 						inputName, inputPrice));
 
-				System.out.println(standardList.getSize());
-			}
-
-			if (inputCommand.equals("del")) {
-				int inputIndex = input.nextInt();
-				standardList.delProduct(inputIndex);
-			}
-
-			if (inputCommand.equals("total")) {
-				standardList.listProducts();
-				standardList.totalCost();
-			}
-
-			if (inputCommand.equals("end")) {
-				standardList.listProducts();
-				standardList.totalCost();
-				input.close();
+				System.out.println(standardBasket.getSize());
 				break;
+
+			case "del":
+				int inputIndex = input.nextInt();
+				standardBasket.delProduct(inputIndex);
+				break;
+
+			case "total":
+				standardBasket.listProducts();
+				standardBasket.totalCost();
+				break;
+
+			case "end":
+				standardBasket.listProducts();
+				standardBasket.totalCost();
+				input.close();
+				break outerLoop;
+				
 			}
 		}
 	}
